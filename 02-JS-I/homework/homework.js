004adc04385ad9c32079d293ab1bd2ff0b2a9463
Marian <marian.ahu.nob@gmai.com>
2022-04-22 01:54:02 -0300

    m

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -226,7 +226,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  return euro * 1;
+  return euro * 1.2;
 }
 
 
@@ -236,16 +236,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.lenght === 1) {
-    if (letra === "a" | letra === "e" | letra === "i" | letra === "o" | letra === "u"){
-      return "Es Vocal";
-    }else {
-      return "Dato incorrecto";
-    }
-  } else (letra.lenght > 1) {
-    return "Dato incorrecto";
-  }
-}
+  
 
 // No modificar nada debajo de esta línea
 // --------------------------------
