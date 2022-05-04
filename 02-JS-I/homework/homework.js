fe15578b482e2965f3c7e78ad481fc924b557c0f
Marian <marian.ahu.nob@gmail.com>
2022-05-04 15:53:24 -0300

    im back

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -232,11 +232,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if ( "a", "e", "i", "o", "u"){
-    return "Es vocal";
-  } else {
-    return "Dato Incorrecto";
-  }
+  
 }
 
 
