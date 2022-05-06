f8841ccd8f135ffac15029be8fec36cfee3fffed
Marian <marian.ahu.nob@gmail.com>
2022-05-06 18:55:36 -0300

    paso dos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -116,28 +116,23 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
  
   // Tu código:
-if ( numero / 1 === numero) {
-  return true;
-}else{
-  return false;
-}
-}
+if (numero % 1 === 0 ) return true;
 
 function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if (numero % 3 === 0) {
+  if ( numero % 3 === 0 && numero % 5 === 0) {
+    return "fizzbuzz";
+  }
+  else if (numero % 3 === 0) {
     return "fizz";
   }
   else if (numero % 5 === 0){
     return "buzz";
-  }
-  else if ( numero % 15 === 0) {
-    return "fizzbuzz"
   } else {
-    return numero
+    return numero;
   }
 }
 
