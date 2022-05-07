d6afc1e40a58bad7b280fe1ac4af6acfb67ff75f
Marian <marian.ahu.nob@gmail.com>
2022-05-06 23:19:58 -0300

    E

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -193,18 +193,22 @@ function esVerdadero(valor) {
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
- if (valor === true) {
-   return "Soy verdadero";
- } else {
-   return "Soy falso";
- }
+  if (valor === true) {
+    return "Soy verdadero";
+  } else {
+    return "Soy falso";
+  }
 }
 
 function tablaDelSeis() {
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-
+  var tablaDelSeis = [];
+  for (i = 1; i === 60; i++) {
+    tablaDelSeis.append(i * 6)
+  }
+  return tablaDelSeis;
 }
 
 function tieneTresDigitos(numero) {
