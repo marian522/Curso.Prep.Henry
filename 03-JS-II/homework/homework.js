3fe76eacac2ee8dad891659af39c0a0fdf2f8121
Marian <marian.ahu.nob@gmail.com>
2022-05-06 23:00:26 -0300

    E

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -157,7 +157,7 @@ function operadoresLogicos(num1, num2, num3) {
     return "Número 1 es mayor y positivo";
   }
   else if (num3 > num1 && num3 > num2) {
-    return num3++;
+    return ++num3;
   } else {
     return false;
   }
