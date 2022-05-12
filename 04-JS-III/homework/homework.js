c5d4585d2913a09cde27e4349d98e4b3c360ba2f
Marian <marian.ahu.nob@gmail.com>
2022-05-12 17:14:44 -0300

    Fase tres

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -93,10 +93,10 @@ function promedioResultadosTest(resultadosTest) {
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
   var total = 0;
-  for (var i = 0; i < resultsTest.length; i++) {
-    total = total + resultsTest[i];
+  for (var i = 0; i < resultadoTest.length; i++) {
+    total = total + resultadoTest[i];
   }
-  var AverageTotal = total / resultsTest.length;
+  var AverageTotal = total / resultadoTest.length;
 
   return AverageTotal;
 }
@@ -119,10 +119,7 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo) {
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  var mayor = 0
-  for (var i = 0; i < arreglo.length; i++) {
-    if (arreglo[i] > 18)
-}
+  
 }
 
 
