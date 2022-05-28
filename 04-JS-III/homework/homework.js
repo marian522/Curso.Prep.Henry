9be3324d83dd2224a8fa2640d5e38cb32a4c5197
Marian <marian.ahu.nob@gmail.com>
2022-05-28 01:24:24 -0300

    Fase tres

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -115,6 +115,12 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if ( arguments.length < 1 ) return 0;
+  var producto = 1
+  for ( var i = 0; i<arguments.length; i++){
+    producto = producto * arguments[i]
+  }
+  return producto
 }
 
 
