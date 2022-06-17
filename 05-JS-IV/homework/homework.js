63dc6aeb5909896135056885ea029d2f32852862
Marian <marian.ahu.nob@gmail.com>
2022-06-17 19:53:45 -0300

    entrenamiento

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -47,7 +47,7 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
-  delete objeto.[unaPropiedad];
+  delete objeto[unaPropiedad];
   return objeto;
 }
 
