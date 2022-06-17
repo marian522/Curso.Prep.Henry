d5edd8a580279f83e98458b3a8c70d18e7f16b87
Marian <marian.ahu.nob@gmail.com>
2022-06-17 20:07:13 -0300

    entrenamiento

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -106,7 +106,7 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
-  usuario.password = nuevagPassword;
+  usuario.password = nuevaPassword;
   return usuario;
 }
 
